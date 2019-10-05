062a0380afd884bf442066385938aee888463349
Brett Viren <brett.viren@gmail.com>
2019-10-05 12:53:49 -0400

    Add more zproject generation

---
+++ include/zperfmq.hpp
@@ -0,0 +1,16 @@
+/*  =========================================================================
+    zperfmq - ZeroMQ performance measurement tool
+
+    GPL 3.0
+    =========================================================================
+*/
+
+#ifndef ZPERFMQ_HPP_H_INCLUDED
+#define ZPERFMQ_HPP_H_INCLUDED
+
+//  Include the project library file
+#include "zperfmq_library.hpp"
+
+//  Add your own public definitions here, if you need them
+
+#endif
